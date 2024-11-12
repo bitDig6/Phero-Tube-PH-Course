4a694fdef128da9a71412e206908336990183ec2
bitDig6 <ummenabilajalal@gmail.com>
2024-11-12 06:52:44 +0600

    added verified badge using rendering conditions

--- script/video.js
+++ script/video.js
@@ -34,14 +34,26 @@ const displayVideos = (videos) => {
         const card = document.createElement('div');
         card.classList = "card card-compact";
         card.innerHTML = `
-            <figure class="h-[200px]">
-                <img class="h-full w-full object-cover" src=${video.thumbnail} alt="Shoes" />
+            <figure class="h-[200px] relative">
+                <img class="h-full w-full object-cover" src=${video.thumbnail} alt="thumbnail" />
+                <span class="absolute right-2 bottom-2 bg-black text-white rounded p-1">${video.others.posted_date}</span>
             </figure>
-            <div class="card-body">
-                <h2 class="card-title">Shoes!</h2>
-                <p>If a dog chews shoes whose shoes does he choose?</p>
-                <div class="card-actions justify-end">
-                <button class="btn btn-primary">Buy Now</button>
+            <div class="px-0 py-4 flex gap-2">
+                <div>
+                    <img class="w-10 h-10 rounded-full object-cover" src="${video.authors[0].profile_picture}"/>
+                </div>
+                <div>
+                    <h2 class="font-bold">${video.title}</h2>
+                    <div class="flex items-center gap-2">
+                        <p>${video.authors[0].profile_name}</p>
+                        
+                        ${
+                            video.authors[0].verified === true 
+                            ?  `<img class="w-5" src="https://img.icons8.com/?size=48&id=D9RtvkuOe31p&format=png"/>`
+                            : ""
+                        }
+                    </div>
+                    <p>${video.others.views} views</p>
                 </div>
             </div>    
         `;
---
+++ test.js
@@ -0,0 +1,9 @@
+const isVerified = "";
+
+// if(isVerified){
+//     console.log('User is verified');
+// }else{
+//     console.log('User is not verified');
+// }
+
+console.log(`${isVerified === true ? "user verified" : "user is not verified"}`);
